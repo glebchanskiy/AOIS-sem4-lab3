8e205e6a498ec650759869fdc707f1adfee6ca16
Levkov Gleb <kiss-shot-heart-under-blade@yandex.by>
2023-05-06 13:09:01 +0300

    update: add several tests

--- app/src/test/java/org/glebchanskiy/aoislab3/logicparser/miniz/impls/CalculationMethodTest.java
+++ app/src/test/java/org/glebchanskiy/aoislab3/logicparser/miniz/impls/CalculationMethodTest.java
@@ -1,5 +1,7 @@
 package org.glebchanskiy.aoislab3.logicparser.miniz.impls;
 
+import org.glebchanskiy.aoislab3.logicparser.FormulasOperations;
+import org.glebchanskiy.aoislab3.logicparser.LogicParser;
 import org.glebchanskiy.aoislab3.logicparser.minimalization.impls.CalculationMethod;
 import org.glebchanskiy.aoislab3.logicparser.util.FormulaType;
 import org.junit.jupiter.api.BeforeAll;
@@ -12,65 +14,72 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculationMethodTest {
     static CalculationMethod minimizator;
-    static List<List<String>> example1;
-    static List<List<String>> example1Result;
-    static List<List<String>> example2;
-    static List<List<String>> example2Result;
+
+    static List<List<String>> example0pcnf;
+    static String example0Result;
+    static List<List<String>> example1pdnf;
+    static String example1Result;
+    static List<List<String>> example2pcnf;
+    static String example2Result;
 
     @BeforeAll
     static void setUp() {
         minimizator = new CalculationMethod();
 
-        // (!a !b c) (!a b !c) (!a b c) (a !b c)
-        example1 = new ArrayList<>(List.of(
-                new ArrayList<>(List.of("!a", "!b", "c")),
-                new ArrayList<>(List.of("!a", "b", "!c")),
-                new ArrayList<>(List.of("!a", "b", "c")),
-                new ArrayList<>(List.of("a", "!b", "c"))
-        ));
+        example0pcnf = FormulasOperations.getPcnfAsList(LogicParser.parse("(A|B|C)&(A|!B|C)&(!A|B|C)"));
+        example0Result = "[[B, C], [A, C]]";
 
-        // (!a !b !c) (!a !b c) (!a b !c) (!a b c)
-        example2 = new ArrayList<>(List.of(
-                new ArrayList<>(List.of("!a", "!b", "!c")),
-                new ArrayList<>(List.of("!a", "!b", "c")),
-                new ArrayList<>(List.of("!a", "b", "!c")),
-                new ArrayList<>(List.of("!a", "b", "c"))
-        ));
+        example1pdnf = FormulasOperations.getPdnfAsList(LogicParser.parse("(!A&B&C)|(A&!B&C)|(A&B&C)"));
+        example1Result = "[[B, C], [A, C]]";
+
+        example2pcnf = FormulasOperations.getPcnfAsList(LogicParser.parse("(A|B|C)&(A|B|!C)&(A|!B|C)&(!A|B|C)&(!A|!B|C)"));
+        example2Result = "[[A, B], [C]]";
+
+    }
+
+    @Test
+    void minimiseTest0() {
+        assertEquals(
+                example0Result,
+                minimizator.minimise(example0pcnf, FormulaType.PCNF).toString()
+        );
     }
 
     @Test
     void minimiseTest1() {
         assertEquals(
-                "[[!a, c], [!b, c], [!a, b]]",
+                example1Result,
                 minimizator.minimise(
-                        example1,
-                        FormulaType.PCNF
+                        example1pdnf,
+                        FormulaType.PDNF
                 ).toString());
     }
 
     @Test
     void minimiseTest2() {
         assertEquals(
-                "[[!a, !b], [!a, !c], [!a, c], [!a, b]]",
+                example2Result,
                 minimizator.minimise(
-                        example2,
-                        FormulaType.PDNF
+                        example2pcnf,
+                        FormulaType.PCNF
                 ).toString());
     }
 
     @Test
     void gluingTest1() {
+        assertEquals(
+                "[[A, B], [B, C], [A, C], [!B, C], [!A, C]]",
+                minimizator.gluing(
+                        example2pcnf
+                ).toString());
     }
 
     @Test
     void gluingTest2() {
-    }
-
-    @Test
-    void createValuesMapTest1() {
         assertEquals(
-                "{!a=1, a=0, !b=1, b=0, !c=0, c=1}",
-                minimizator.createValuesMap(
-                        example1.get(0), FormulaType.PDNF).toString());
+                "[[B, C], [A, C]]",
+                minimizator.gluing(
+                        example1pdnf
+                ).toString());
     }
 }
